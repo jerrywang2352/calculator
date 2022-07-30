b97f44d655a51b4edacf1fab68f6d9d7e8a69b76
Jerry Wang <jerry.wang2352@gmail.com>
2022-07-30 01:20:46 -0400

    Write basic math functions

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function operate(a,b,operator) {
+    if(operator === "add") {
+        return add(a,b);
+    } else if(operator === "subtract") {
+        return subtract(a,b);
+    } else if(operator === "multiply") {
+        return multiply(a,b);
+    } else if(operator === "divide") {
+        return divide(a,b); 
+    }
+}
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+} 
+
+function multiply(a,b) {
+    return a*b;
+} 
+
+function divide(a,b) {
+    return a/b; 
+}
