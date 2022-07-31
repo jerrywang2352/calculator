730bf32f24b8d7c14c77b6f05c9de4c9043a99a5
Jerry Wang <jerry.wang2352@gmail.com>
2022-07-30 22:23:46 -0400

    Display input expression on UI

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const buttons = document.querySelectorAll("button"); 
-buttons.forEach((button) => {
-    button.addEventListener("click",() => {
-        alert("hello world");
+const numbers = document.querySelectorAll("#number");
+const operators = document.querySelectorAll("#operator");  
+const AC = document.querySelector(".AC"); 
+const C = document.querySelector(".C"); 
+const equal = document.querySelector(".equal"); 
+const input = document.querySelector(".input"); 
+const results = document.querySelector(".results"); 
+
+let expression = [];
+let currNum = ""; 
+
+//show user number input  
+numbers.forEach((button) => {
+    button.addEventListener("click",(e) => {
+        currNum += e.target.innerText;
+        input.innerText = expression.join(" ") + " "+ currNum; 
+        console.log(expression.join(" "));  
     });
 }); 
+
+//show user basic math operator input 
+operators.forEach((button) => {
+    button.addEventListener("click",(e) => {
+        if (currNum.length != 0) {
+            expression.push(currNum); 
+            currNum = ""; 
+        }
+        if ("+−×÷".split("").includes(expression[expression.length-1])) {
+            expression[expression.length-1] = e.target.innerText;
+        } else {
+            expression.push(e.target.innerText); 
+        }
+        input.innerText = expression.join(" ");
+        console.log(expression.join(" ")); 
+    });
+}); 
+
+//Reset currNum 
+C.addEventListener("click",() => {
+    currNum = ""; 
+});
+
+//Reset Calculator 
+AC.addEventListener("click",() => {
+    currNum = "";
+    expression = []; 
+    input.innerText = "0"; 
+    results.innerText = ""; 
+})
 
 
 function operate(a,b,operator) {
